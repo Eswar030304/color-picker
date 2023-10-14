39a7e19ed01420d80532d7fd9712db81f1e2d5aa
Pravallika283 <tpravallika283@gmail.com>
2023-10-15 00:29:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {SketchPicker} from 'react-color'
+import { useState } from "react";
 
 function App() {
+
+  const [currentColor , setCurrentColor] =useState("#ffff")
+  const hanleOnChange = (color)=>{
+    setCurrentColor()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>React color picker</h1>
+
+      <SketchPicker
+      color={currentColor}
+      onChangeComplete={hanleOnChange(color)}
+      />
+
     </div>
   );
 }
